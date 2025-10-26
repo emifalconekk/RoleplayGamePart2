21920b606aa0663730d18fa059903bafecd0f2b8
Matheus Hladki <MATHEUS.HLADKI@correo.ucu.edu.uy>
2025-10-26 20:21:17 -0300

    Primeros 2 personajes

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,14 +3,24 @@
 SpellsBook book = new SpellsBook();
 book.Spells = new Spell[]{ new Spell() };
 
-Wizard gandalf = new Wizard("Gandalf");
+Wizard gandalf = new Wizard("Gandalf", "Heroe");
 gandalf.Staff = new Staff();
 gandalf.SpellsBook = book;
 
-Dwarf gimli = new Dwarf("Gimli");
+Dwarf gimli = new Dwarf("Gimli", "Heroe");
 gimli.Axe = new Axe();
 gimli.Helmet = new Helmet();
 gimli.Shield = new Shield();
+
+Archer robin = new Archer("Robin", "Villano");
+robin.Bow = new Bow();
+robin.Helmet = new Helmet();
+
+Knight garen = new Knight("Garen", "Villano");
+garen.Sword = new Sword();
+garen.Shield = new Shield();
+garen.Armor = new Armor();
+
 
 Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
 Console.WriteLine($"Gandalf attacks Gimli with ⚔️ {gandalf.AttackValue}");
